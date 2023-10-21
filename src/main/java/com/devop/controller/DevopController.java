7dc00d58c7c3154e07d879d26592bec85e3f0bb9
Sneka-Mariyappan <iamsnekamariyappan@gmail.com>
2023-10-21 16:47:25 +0530

    Added

--- src/main/java/com/devop/controller/DevopController.java
+++ src/main/java/com/devop/controller/DevopController.java
@@ -1,5 +1,13 @@
 package com.devop.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class DevopController {
+
+	@GetMapping(path="/set1")
+	public String getValue() {
+		return "HelloWorld";
+	}
+	
 
 }
